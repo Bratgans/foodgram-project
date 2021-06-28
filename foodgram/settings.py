d6d656f567da0f965d51cb8c2d8f8f8fea207dcb
Bratgans <bratgans@gmail.com>
2021-06-28 15:03:18 +0300

    deploy_v1.2

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -115,10 +115,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = [
-#     'static_files',
-# ]
-
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
